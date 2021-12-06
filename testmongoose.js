18a219ea03fbb47a15d1b02c3530134a331616e0
CJJ <caijingjing2001@163.com>
2021-12-05 16:24:57 -0800

    First commit

---
+++ testmongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const schema = {
+    name: String,
+    age: Number,
+    health:String
+}
+const Cat = mongoose.model('Cat', schema);
+
+// const kitty = new Cat({ name1: 'testZildjian1' });
+// kitty.save().then(() => console.log('testmeow1'));
+// const kitty1 = new Cat({name:"hellokitty1",age:5,health:"good"})
+// kitty1.save().then(() => console.log("new schema writed."))
+
+Cat.find({name:"hellokitty"},(err,data)=>{console.log(data[0]._doc.name)})
